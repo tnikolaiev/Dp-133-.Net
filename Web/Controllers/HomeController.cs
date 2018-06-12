a0220d0a39d4e5efef18c7de034cdbd2d60e163d
dhuli <dhuli>
2018-06-12 18:41:19 +0300

    Add logger to HomeController

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -4,13 +4,22 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogInformation("Index page succesfully loaded");
             return View();
         }
 
