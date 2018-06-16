b958f7160ac275878e505739a5b14d98a1b0e8e3
Viacheslav Rud <rudviacheslavv@gmail.com>
2018-06-16 13:08:50 +0300

    Created IRepository and IUnitOfWork interfaces.

---
+++ DAL/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ras.DAL.Entity;
+
+namespace Ras.DAL
+{
+    interface IUnitOfWork
+    {
+        IRepository<Group> GroupsRepository { get; }
+
+        IRepository<Student> StudentsRepository { get; }
+
+        IRepository<GroupInfo> GroupsInfoRepsitory { get; }
+
+        IRepository<GroupStage> GroupStagesRepository { get; }
+
+        IRepository<LanguageTranslations> LanguageTranslationsRepository { get; }
+
+        IRepository<Direction> DirectionsRepository { get; }
+
+        IRepository<Technology> TechnologiesRepository { get; }
+
+        void SaveChanges();
+    }
+}
