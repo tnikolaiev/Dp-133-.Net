26b897e3a2402d44f8a571893e86af2687eb0929
Viacheslav Rud <rudviacheslavv@gmail.com>
2018-06-21 17:28:25 +0300

    Created IGroupService interface.

---
+++ BLL/IGroupService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ras.BLL.DTO;
+
+namespace Ras.BLL
+{
+    interface IGroupService
+    {
+        GroupDTO GetById(int id);
+
+        GroupDTO Create(GroupDTO group);
+
+        GroupDTO Update(GroupDTO group);
+
+    }
+}
