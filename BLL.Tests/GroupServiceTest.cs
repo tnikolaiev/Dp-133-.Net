ed29f2c017af02d4cf4e3dc2aee18927a3d28bf3
dhuli <dhuli>
2018-06-23 11:02:48 +0300

    Created empty GroupServiceTest

---
+++ BLL.Tests/GroupServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ras.BLL.Implementation;
+using Ras.DAL;
+using Ras.BLL.DTO;
+using Ras.DAL.Entity;
+using Moq;
+
+namespace Ras.BLL.Tests
+{
+    [TestClass]
+    public class GroupServiceTest
+    {
+        [TestMethod]
+        public void Create_New_Group()
+        {
+
+        }
+    }
+}
