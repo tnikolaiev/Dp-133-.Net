85b830ebb0ae5a63affd6d6329d6825b3e3a3440
Viacheslav Rud <rudviacheslavv@gmail.com>
2018-06-23 18:04:17 +0300

    Added new tests.

--- BLL.Tests/StudentServiceTest.cs
+++ BLL.Tests/StudentServiceTest.cs
@@ -1,26 +1,65 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ras.BLL.DTO;
 using Ras.BLL.Implementation;
 using Ras.DAL;
-using Ras.BLL.DTO;
-using Moq;
 using Ras.DAL.Entity;
+using System.Collections.Generic;
 
 namespace Ras.BLL.Tests
 {
     [TestClass]
     public class StudentServiceTest
     {
+        private IStudentService Initialize()
+        {
+            var mock = new Mock<IUnitOfWork>();
+            var studentService = new StudentService(mock.Object);
+
+            var student = new Student
+            {
+                Id = 3,
+                UserId = 1,
+                User = new User { Id = 1 },
+                Group = new Group(),
+            };
+
+            mock.Setup(x => x.StudentsRepository.Read(It.Is<int>(a => a == (3)))).Returns(student);
+            mock.Setup(x => x.StudentsRepository.Delete(It.Is<int>(a => a == 3))).Callback(() =>
+                        mock.Setup(x => x.StudentsRepository.Read(It.Is<int>(a => a == (3)))).Returns<Student>(null));
+
+            mock.Setup(x => x.StudentsRepository.Upate(It.Is<Student>(a => a.Id == 3)))
+                .Returns(student);
+
+            return studentService;
+        }
+
+        [TestMethod]
+        public void Delete_ExistStudent_ReturnsWithSuchId()
+        {
+            var studentService = Initialize();
+            studentService.Delete(3);
+
+            Assert.ThrowsException<System.ArgumentException>(()=> studentService.GetById(3));
+        }
+
+        [TestMethod]
+        public void GetById_Exist_ReturnsStudentWithSuchId()
+        {
+            var studentService = Initialize();
+
+            var student = studentService.GetById(3);
+
+            Assert.AreEqual(3, student.Id);
+
+        }
+
         [TestMethod]
         public void Update_Existed_Student()
         {
-            Mock<IUnitOfWork> mock = new Mock<IUnitOfWork>();
-            StudentService studentService = new StudentService(mock.Object);
-            mock.Setup(x => x.StudentsRepository.Read(It.IsAny<int>())).Returns(new Student());
-            mock.Setup(x => x.StudentsRepository.Upate(It.IsAny<Student>())).Returns(new Student() { User = new User(),Group=new Group(),UserId=0 });
-            mock.Setup(x => x.SaveChanges());
+            var studentService = Initialize();
 
-            var result = studentService.UpdateStudent(new StudentDTO());
+            var result = studentService.UpdateStudent(new StudentDTO { Id = 3 });
 
             Assert.IsNotNull(result);
         }
@@ -28,8 +67,8 @@ namespace Ras.BLL.Tests
         [TestMethod]
         public void Update_No_Existed_Student()
         {
-            Mock<IUnitOfWork> mock = new Mock<IUnitOfWork>();
-            StudentService studentService = new StudentService(mock.Object);
+            var mock = new Mock<IUnitOfWork>();
+            var studentService = new StudentService(mock.Object);
             mock.Setup(x => x.StudentsRepository.Read(It.IsAny<int>())).Returns<Student>(null);
 
             var result = studentService.UpdateStudent(new StudentDTO());
@@ -40,8 +79,8 @@ namespace Ras.BLL.Tests
         [TestMethod]
         public void Update_Existed_FeedBack()
         {
-            Mock<IUnitOfWork> mock = new Mock<IUnitOfWork>();
-            StudentService studentService = new StudentService(mock.Object);
+            var mock = new Mock<IUnitOfWork>();
+            var studentService = new StudentService(mock.Object);
             mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns(new Feedback());
             mock.Setup(x => x.FeedbacksRepository.Upate(It.IsAny<Feedback>())).Returns(new Feedback());
             mock.Setup(x => x.SaveChanges());
@@ -54,8 +93,8 @@ namespace Ras.BLL.Tests
         [TestMethod]
         public void Update_No_Existed_FeedBack()
         {
-            Mock<IUnitOfWork> mock = new Mock<IUnitOfWork>();
-            StudentService studentService = new StudentService(mock.Object);
+            var mock = new Mock<IUnitOfWork>();
+            var studentService = new StudentService(mock.Object);
             mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns<Feedback>(null);
 
             var result = studentService.UpdateFeedback(new FeedbackDTO());
