e5e3f906cf59fcc5298ef7cc136699c7eea457c9
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2018-06-24 20:57:32 +0300

    Create GroupInfoViewModel and GroupInfoController with Add, Update, GetInfo methods

---
+++ Web/Controllers/GroupInfoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ras.BLL;
+using Ras.Web.Models;
+using AutoMapper;
+using Ras.BLL.DTO;
+
+namespace Web.Controllers
+{
+    public class GroupInfoController : Controller
+    {
+        IGroupService groupService;
+        IMapper groupMapper;
+        IMapper groupDtoMapper;
+
+        public GroupInfoController(IGroupService service)
+        {
+            groupService = service;
+            groupMapper = new MapperConfiguration(cfg => cfg.CreateMap<GroupDTO, GroupInfoViewModel>()).CreateMapper();
+            groupDtoMapper = new MapperConfiguration(cfg => cfg.CreateMap<GroupInfoViewModel, GroupDTO>()).CreateMapper();
+        }
+
+        //return Create ViewModel object?
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public GroupInfoViewModel GetGroupInfo(int id)
+        {
+            var group = groupMapper.Map<GroupDTO, GroupInfoViewModel>(groupService.GetById(id));
+            return group;
+        }
+
+        [HttpPost]
+        public void EditGroupInfo([FromBody] GroupInfoViewModel group)
+        {
+            var groupForEdit = groupDtoMapper.Map<GroupInfoViewModel, GroupDTO>(group);
+            groupService.Update(groupForEdit);
+        }
+
+        [HttpPost]
+        public void AddGroupInfo([FromBody] GroupInfoViewModel group)
+        {
+            var groupNew = groupDtoMapper.Map<GroupInfoViewModel, GroupDTO>(group);
+            groupService.Create(groupNew);
+        }
+    }
+}
---
+++ Web/Models/GroupInfoVIewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ras.Web.Models
+{
+    public class GroupInfoViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CrmGroup { get; set; }               //what is this?
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string City { get; set; }
+        public string Direction { get; set; }
+        public string Technology { get; set; }
+        public string Stage { get; set; }
+        public string NameForSite { get; set; }         //where this info from? it is absent in DTO
+        public string PaymentStatus { get; set; }       //where this info from? it is absent in DTO
+        public string CommonDirection { get; set; }       //where this info from? it is absent in DTO
+        public string Profile { get; set; }       //where this info from? it is absent in DTO
+        public int AmountStudentForGraduate { get; set; }       //where this info from? it is absent in DTO
+        public int AmountStudentForEnrollment { get; set; }       //where this info from? it is absent in DTO
+        public int AmountStudenActual { get; set; }       //where this info from? it is absent in DTO
+    }
+}
