77157ed40ff49ab70d700da4e4c2b6e765213c0b
IhnatenkoDenys <den4ik6645@gmail.com>
2018-06-26 09:49:14 +0300

    StudentController was created

---
+++ Web/Controllers/StudentsController.cs
@@ -0,0 +1,66 @@
+﻿using Ras.BLL;
+using Microsoft.AspNetCore.Mvc;
+using Ras.Web.Models;
+using AutoMapper;
+using Ras.BLL.DTO;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        private IStudentService studentService;
+
+        private readonly IMapper userDTOMapper;
+        private readonly IMapper studentMapper;
+        private readonly IMapper studentDTOMapper;
+
+        public StudentsController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+            userDTOMapper = new MapperConfiguration(cfg => cfg.CreateMap<UserViewModel, UserDTO>()).CreateMapper();
+            studentMapper = new MapperConfiguration(cfg => cfg.CreateMap<StudentDTO, StudentViewModel>()).CreateMapper();
+            studentDTOMapper = new MapperConfiguration(cfg => cfg.CreateMap<StudentViewModel, StudentDTO>()).CreateMapper();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]UserViewModel user, int groupId)
+        {
+            var userMap = userDTOMapper.Map<UserViewModel, UserDTO>(user);
+
+            if (ModelState.IsValid)
+            {
+                studentService.CreateStudent(userMap, groupId);
+                return Ok(user);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("{id}")]
+        public StudentViewModel Get(int id)
+        {
+            StudentViewModel student = studentMapper.Map<StudentDTO, StudentViewModel>(studentService.GetById(id));
+
+            return student;
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody]StudentViewModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                var studentDTO = studentDTOMapper.Map<StudentViewModel, StudentDTO>(student);
+                studentService.UpdateStudent(studentDTO);
+                return Ok(student);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            studentService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Web/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Ras.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ras.Web.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string FirtstName { get; set; }
+        public string LastName { get; set; }
+        public int? EnglishLevel { get; set; }
+        public string Phone { get; set; }
+        public string Salt { get; set; }
+        public string Password { get; set; }
+    }
+}
