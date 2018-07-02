5aba3c74f03a57543b00a802c7e9f2cc3e0c68c9
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2018-07-02 13:50:30 +0300

    Added DTO and interface for DictionatiesService

---
+++ BLL/DTO/DictionariesDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Ras.BLL.DTO
+{
+    public class DictionariesDTO
+    {
+        public DictionariesDTO()
+        {
+            Cities = new Dictionary<int, string>();
+            Directions = new Dictionary<int, string>();
+            Technologies = new Dictionary<int, string>();
+            Stages = new Dictionary<int, string>();
+            NamesForSite = new Dictionary<int, string>();
+            PaymentStatuses = new Dictionary<int, string>();
+            Profiles = new Dictionary<int, string>();
+        }
+
+        public Dictionary<int, string> Cities { get; set; }
+        public Dictionary<int, string> Directions { get; set; }     //CommonDirection
+        public Dictionary<int, string> Technologies { get; set; }
+        public Dictionary<int, string> Stages { get; set; }
+        public Dictionary<int, string> NamesForSite { get; set; }
+        public Dictionary<int, string> PaymentStatuses { get; set; }
+        public Dictionary<int, string> Profiles { get; set; }
+    }
+}
---
+++ BLL/Implementation/DictionariesService.cs
@@ -0,0 +1,12 @@
+﻿using Ras.BLL.DTO;
+
+namespace Ras.BLL.Implementation
+{
+    public class DictionariesService : IDictionariesService
+    {
+        public DictionariesDTO GetGroupInfoDictionaries()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
