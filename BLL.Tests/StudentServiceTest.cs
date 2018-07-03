9ac562079a4a7313f71dd559849fe6f58dfd3bf3
Viacheslav Rud <rudviacheslavv@gmail.com>
2018-07-03 12:09:55 +0300

    Refactored test. Added null checks in service.

--- BLL.Tests/StudentServiceTest.cs
+++ BLL.Tests/StudentServiceTest.cs
@@ -1,74 +1,78 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Ras.BLL.DTO;
+using Ras.BLL.Exeptions;
 using Ras.BLL.Implementation;
 using Ras.DAL;
 using Ras.DAL.Entity;
-using System.Collections.Generic;
-using Ras.BLL.Exeptions;
-using Ras.DAL.Implementation;
 
 namespace Ras.BLL.Tests
 {
     [TestClass]
     public class StudentServiceTest
     {
-        private static StudentService studentService;
-        private static Mock<IUnitOfWork> mock;
-        private static Student student;
+        private static StudentService StudentService;
+        private static readonly Mock<IUnitOfWork> Mock = new Mock<IUnitOfWork>();
+
+        private static readonly Student Student = new Student
+        {
+            Id = 3,
+            UserId = 1,
+            User = new User {Id = 1},
+            Group = new Group()
+        };
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context)
         {
-            mock = new Mock<IUnitOfWork>();
-            studentService = new StudentService(mock.Object);
-            student = new Student
-            {
-                Id = 3,
-                UserId = 1,
-                User = new User { Id = 1 },
-                Group = new Group(),
-            };
+            Mock.Setup(x => x.StudentsRepository.Read(It.Is<int>(a => a == 3))).Returns(Student);
+            Mock.Setup(x => x.StudentsRepository.Delete(It.Is<int>(a => a == 3))).Callback(() =>
+                                                                                               Mock.Setup(x => x.StudentsRepository.Read(
+                                                                                                              It.Is<int>(a => a == 3)))
+                                                                                                   .Returns<Student>(null));
+            Mock.Setup(x => x.StudentsRepository.Upate(It.Is<Student>(a => a.Id == 3)))
+                .Returns(Student);
+
+            Mock.Setup(x => x.EnglishLevelsRepository.All).Returns(new List<EnglishLevel>().AsQueryable());
+            Mock.Setup(x => x.StudentStatusesRepository.All).Returns(new List<StudentStatus>().AsQueryable());
+
+            Mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns(new Feedback());
+            Mock.Setup(x => x.FeedbacksRepository.All).Returns(new[] { new Feedback() }.AsQueryable());
+
+            Mock.Setup(x => x.FeedbacksRepository.Upate(It.IsAny<Feedback>())).Returns(new Feedback());
+            Mock.Setup(x => x.SaveChanges());
+
+            Mock.Setup(x => x.CharacteristicsRepository.All).Returns(new[] { new Characteristic() }.AsQueryable());
+
+            Mock.Setup(x => x.MarksRepository.All).Returns(new[] { new Mark() }.AsQueryable());
+
+            StudentService = new StudentService(Mock.Object);
         }
-
-        private void Initialize()
-        {
-            mock.Setup(x => x.StudentsRepository.Read(It.Is<int>(a => a == (3)))).Returns(student);
-            mock.Setup(x => x.StudentsRepository.Delete(It.Is<int>(a => a == 3))).Callback(() =>
-                        mock.Setup(x => x.StudentsRepository.Read(It.Is<int>(a => a == (3)))).Returns<Student>(null));
-
-            mock.Setup(x => x.StudentsRepository.Upate(It.Is<Student>(a => a.Id == 3)))
-                .Returns(student);
-        }
-
-        
 
         [TestMethod]
         public void Delete_ExistStudent_ReturnsWithSuchId()
         {
-            Initialize();
-            studentService.Delete(3);
+            StudentService.Delete(3);
 
-            Assert.ThrowsException<StudentNotFoundException>(()=> studentService.GetById(3));
+            Assert.ThrowsException<StudentNotFoundException>(() => StudentService.GetById(3));
         }
 
         [TestMethod]
         public void GetById_Exist_ReturnsStudentWithSuchId()
         {
-            Initialize();
-
-            var student = studentService.GetById(3);
+            var student = StudentService.GetById(3);
 
             Assert.AreEqual(3, student.Id);
-
         }
 
         [TestMethod]
         public void Update_Existed_Student()
         {
-            mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns<Feedback>(null);
+            Mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns<Feedback>(null);
 
-            var result = studentService.UpdateStudent(new StudentDTO { Id = 3 });
+            var result = StudentService.UpdateStudent(new StudentDTO {Id = 3});
 
             Assert.IsNotNull(result);
         }
@@ -77,19 +81,15 @@ namespace Ras.BLL.Tests
         [ExpectedException(typeof(StudentNotFoundException))]
         public void Update_No_Existed_Student()
         {
-            mock.Setup(x => x.StudentsRepository.Read(It.IsAny<int>())).Returns<Student>(null);
+            Mock.Setup(x => x.StudentsRepository.Read(It.IsAny<int>())).Returns<Student>(null);
 
-            var result = studentService.UpdateStudent(new StudentDTO());
+            var result = StudentService.UpdateStudent(new StudentDTO());
         }
 
         [TestMethod]
         public void Update_Existed_FeedBack()
         {
-            mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns(new Feedback());
-            mock.Setup(x => x.FeedbacksRepository.Upate(It.IsAny<Feedback>())).Returns(new Feedback());
-            mock.Setup(x => x.SaveChanges());
-
-            var result = studentService.UpdateFeedback(new FeedbackDTO());
+            var result = StudentService.UpdateFeedback(new FeedbackDTO());
 
             Assert.IsNotNull(result);
         }
@@ -98,9 +98,9 @@ namespace Ras.BLL.Tests
         [ExpectedException(typeof(FeedbackExeption))]
         public void Update_No_Existed_FeedBack()
         {
-            mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns<Feedback>(null);
+            Mock.Setup(x => x.FeedbacksRepository.Read(It.IsAny<int>())).Returns<Feedback>(null);
 
-            var result = studentService.UpdateFeedback(new FeedbackDTO());
-        }        
+            var result = StudentService.UpdateFeedback(new FeedbackDTO());
+        }
     }
 }
--- BLL/Implementation/StudentService.cs
+++ BLL/Implementation/StudentService.cs
@@ -51,7 +51,7 @@ namespace Ras.BLL.Implementation
             if (dStudent != null)
             {
                 dStudent.EngGram = student.EnglishGrammar;
-                dStudent.EnglishLevelId = unitOfWork.EnglishLevelsRepository.All.FirstOrDefault(x => x.Name == student.EnglishLevel).EnglishLevelId;
+                dStudent.EnglishLevelId = unitOfWork.EnglishLevelsRepository.All.FirstOrDefault(x => x.Name == student.EnglishLevel)?.EnglishLevelId;
                 dStudent.EntryScore = student.EntryScore;
                 dStudent.ExpertScore = student.ExpertScore;
                 dStudent.FinalBase = student.FinalBase;
@@ -60,18 +60,18 @@ namespace Ras.BLL.Implementation
                 dStudent.IntermTestLang = student.IntermTestLang;
                 dStudent.InterviewerComment = student.InterviewerComment;
                 dStudent.InterviewerScore = student.InterviewerScore;
-                dStudent.StudentStatusId =unitOfWork.StudentStatusesRepository.All.FirstOrDefault(x=>x.Name== student.StudentStatus).Id;
+                dStudent.StudentStatusId =unitOfWork.StudentStatusesRepository.All.FirstOrDefault(x=>x.Name== student.StudentStatus)?.Id;
                 dStudent.TeacherScore = student.TeacherScore;
-                dStudent.Test1 = student.Tests[0];
-                dStudent.Test2 = student.Tests[1];
-                dStudent.Test3 = student.Tests[2];
-                dStudent.Test4 = student.Tests[3];
-                dStudent.Test5 = student.Tests[4];
-                dStudent.Test6 = student.Tests[5];
-                dStudent.Test7 = student.Tests[6];
-                dStudent.Test8 = student.Tests[7];
-                dStudent.Test9 = student.Tests[8];
-                dStudent.Test10 = student.Tests[9];
+                dStudent.Test1 = student.Tests?[0];
+                dStudent.Test2 = student.Tests?[1];
+                dStudent.Test3 = student.Tests?[2];
+                dStudent.Test4 = student.Tests?[3];
+                dStudent.Test5 = student.Tests?[4];
+                dStudent.Test6 = student.Tests?[5];
+                dStudent.Test7 = student.Tests?[6];
+                dStudent.Test8 = student.Tests?[7];
+                dStudent.Test9 = student.Tests?[8];
+                dStudent.Test10 = student.Tests?[9];
                 newStudent = new StudentDTO(unitOfWork.StudentsRepository.Upate(dStudent));
                 unitOfWork.SaveChanges();
                 return newStudent;
@@ -131,23 +131,23 @@ namespace Ras.BLL.Implementation
 
         private void CopyMembers(FeedbackDTO feedback,Feedback creatingFeedback)
         {
-            int? activeCommunicatorId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.ActiveCommunicatorTitle).CharacteristicId;
-            creatingFeedback.ActiveCommunicatorId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == activeCommunicatorId && x.Name == feedback.ActiveCommunicatorCharacteristic).MarkId;
+            int? activeCommunicatorId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.ActiveCommunicatorTitle)?.CharacteristicId;
+            creatingFeedback.ActiveCommunicatorId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == activeCommunicatorId && x.Name == feedback.ActiveCommunicatorCharacteristic)?.MarkId;
 
-            int? gettingThingsDoneId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.GettingThingsDoneTitle).CharacteristicId;
-            creatingFeedback.GettingThingsDoneId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == gettingThingsDoneId && x.Name == feedback.GettingThingsDoneCharacteristic).MarkId;
+            int? gettingThingsDoneId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.GettingThingsDoneTitle)?.CharacteristicId;
+            creatingFeedback.GettingThingsDoneId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == gettingThingsDoneId && x.Name == feedback.GettingThingsDoneCharacteristic)?.MarkId;
 
-            int? learningAbilityId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.LearningAbilityTitle).CharacteristicId;
-            creatingFeedback.LearningAbilityId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == learningAbilityId && x.Name == feedback.LearningAbilityCharacteristic).MarkId;
+            int? learningAbilityId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.LearningAbilityTitle)?.CharacteristicId;
+            creatingFeedback.LearningAbilityId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == learningAbilityId && x.Name == feedback.LearningAbilityCharacteristic)?.MarkId;
 
-            int? passionalInitiativeId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.PassionalInitiativeTitle).CharacteristicId;
-            creatingFeedback.PassionalInitiativeId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == passionalInitiativeId && x.Name == feedback.PassionalInitiativeCharacteristic).MarkId;
+            int? passionalInitiativeId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.PassionalInitiativeTitle)?.CharacteristicId;
+            creatingFeedback.PassionalInitiativeId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == passionalInitiativeId && x.Name == feedback.PassionalInitiativeCharacteristic)?.MarkId;
 
-            int? teamWorkId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.TeamWorkTitle).CharacteristicId;
-            creatingFeedback.TeamWorkId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == teamWorkId && x.Name == feedback.TeamWorkCharacteristic).MarkId;
+            int? teamWorkId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.TeamWorkTitle)?.CharacteristicId;
+            creatingFeedback.TeamWorkId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == teamWorkId && x.Name == feedback.TeamWorkCharacteristic)?.MarkId;
 
-            int? technicalCompetenceId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.TechnicalCompetenceTitle).CharacteristicId;
-            creatingFeedback.TechnicalCompetenceId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == technicalCompetenceId && x.Name == feedback.TechnicalCompetenceCharacteristic).MarkId;
+            int? technicalCompetenceId = unitOfWork.CharacteristicsRepository.All.FirstOrDefault(x => x.Name == feedback.TechnicalCompetenceTitle)?.CharacteristicId;
+            creatingFeedback.TechnicalCompetenceId = unitOfWork.MarksRepository.All.FirstOrDefault(x => x.CharacteristicId == technicalCompetenceId && x.Name == feedback.TechnicalCompetenceCharacteristic)?.MarkId;
 
             creatingFeedback.SummaryComment = feedback.SummaryComment;
         }
