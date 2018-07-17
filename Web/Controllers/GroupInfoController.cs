7dce2dc5a67b01d990291dea8ce5ccf4caaf3835
Anastasiia Blyzniuk <ablyz@softserveinc.com>
2018-07-17 16:18:49 +0300

    Added method`s signatures for employees page

--- Web/Controllers/GroupInfoController.cs
+++ Web/Controllers/GroupInfoController.cs
@@ -4,18 +4,21 @@ using Ras.BLL;
 using Ras.Web.Models;
 using AutoMapper;
 using Ras.BLL.DTO;
+using Ras.Web.Filters;
 
 namespace Web.Controllers
 {
+    [ServiceFilter(typeof(LoggerFilterAttribute))]
+    [ServiceFilter(typeof(CustomExeptionFilterAttribute))]
     [Route("api/[controller]")]
     public class GroupInfoController : Controller
     {
         IGroupService groupService;
-        IDictionariesService dictionariesService;
+        IDictionariesGroupService dictionariesService;
         IMapper groupMapper;
         IMapper groupDtoMapper;
 
-        public GroupInfoController(IGroupService serviceGroup, IDictionariesService serviceDictionaries)
+        public GroupInfoController(IGroupService serviceGroup, IDictionariesGroupService serviceDictionaries)
         {
             groupService = serviceGroup;
             dictionariesService = serviceDictionaries;
@@ -23,14 +26,14 @@ namespace Web.Controllers
             groupDtoMapper = new MapperConfiguration(cfg => cfg.CreateMap<GroupInfoViewModel, GroupDTO>()).CreateMapper();
         }
 
-        [HttpGet("{id}")]
-        public IActionResult GetGroupInfo(int id)
+        [HttpGet("groupInfo/{groupId}")]
+        public IActionResult GetGroupInfo(int groupId)
         {
             try
             {
-                var group = groupMapper.Map<GroupDTO, GroupInfoViewModel>(groupService.GetById(id));
+                var group = groupMapper.Map<GroupDTO, GroupInfoViewModel>(groupService.GetById(groupId));
                 var dictionaries = dictionariesService.GetGroupInfoDictionaries();
-                mapDictionaryFromDto(group, dictionaries);
+                MapDictionaryFromDto(group, dictionaries);
                 return Ok(group);
             }
             catch(ArgumentException)
@@ -39,7 +42,7 @@ namespace Web.Controllers
             }
         }
 
-        [Route("EditGroup")]
+        [Route("editGroup")]
         [HttpPost]
         public IActionResult EditGroupInfo([FromBody] GroupInfoViewModel group)
         {
@@ -55,9 +58,9 @@ namespace Web.Controllers
             }
         }
 
-        [Route("AddGroup")]
+        [Route("createGroup")]
         [HttpPost]
-        public IActionResult AddGroupInfo([FromBody] GroupInfoViewModel group)
+        public IActionResult CreateGroup([FromBody] GroupInfoViewModel group)
         {
             try
             {
@@ -71,11 +74,40 @@ namespace Web.Controllers
             }
         }
 
-        private void mapDictionaryFromDto(GroupInfoViewModel model, DictionariesDTO dictionaries)
+        [Route("getEmployees/{groupId}")]
+        [HttpGet]
+        public IActionResult GetEmployeesForGroup(int groupId)
+        {
+            return Ok();
+        }
+
+        [Route("removeEmployeeFromGroup/{groupId}")]
+        [HttpGet]
+        public IActionResult RemoveEmployeeFromGroup(int employeeId, int groupId)
+        {
+            return Ok();
+        }
+
+        [Route("addEmployeeToGroup/{groupId}")]
+        [HttpGet]
+        public IActionResult AddEmployeeToGroup(int employeeId, int groupId)
+        {
+            return Ok();
+        }
+
+        [Route("updateEmployeeInGroup/{groupId}")]
+        [HttpGet]
+        public IActionResult UpdateEmployeeInGroup()
+        {
+            //TODO Create Teacher View Model
+            //var r = new EmployeeDTO();
+            return Ok();
+        }
+
+        private void MapDictionaryFromDto(GroupInfoViewModel model, DictionariesGroupDTO dictionaries)
         {
             model.Cities = dictionaries.Cities;
             model.Directions = dictionaries.Directions;
-            model.NamesForSite = dictionaries.NamesForSite;
             model.PaymentStatuses = dictionaries.PaymentStatuses;
             model.Profiles = dictionaries.Profiles;
             model.Stages = dictionaries.Stages;
