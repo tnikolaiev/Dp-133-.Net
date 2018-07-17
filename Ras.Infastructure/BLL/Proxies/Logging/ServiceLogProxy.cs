739ffa31605b8f7109557c0412c1c9bb4d05d359
Viacheslav Rud <rudviacheslavv@gmail.com>
2018-07-17 17:24:50 +0300

    Added comments and null check.

--- Ras.Infastructure/BLL/Proxies/Logging/ServiceLogProxy.cs
+++ Ras.Infastructure/BLL/Proxies/Logging/ServiceLogProxy.cs
@@ -8,6 +8,10 @@ using System.Runtime.CompilerServices;
 
 namespace Ras.Infastructure.BLL.Proxies.Logging
 {
+    /// <summary>
+    /// Base for logging proxies. Contains logger and real service. Provides LogBegin and LogEnd methods.
+    /// </summary>
+    /// <typeparam name="TService"></typeparam>
     public abstract class ServiceLogProxy<TService>
     {
         protected readonly ILogger logger;
@@ -15,6 +19,11 @@ namespace Ras.Infastructure.BLL.Proxies.Logging
 
         protected string serviceName;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="service">Another service, methods of that will be wraped with logging.</param>
+        /// <param name="logger">Logger will be used to log.</param>
         public ServiceLogProxy(TService service, ILogger logger)
         {
             if (service is ServiceLogProxy<TService>)
@@ -27,18 +36,32 @@ namespace Ras.Infastructure.BLL.Proxies.Logging
             serviceName = GetType().Name.Replace("LogProxy", "");
         }
 
+        /// <summary>
+        /// Writes message to log with name of service, name of method, arguments in format {parameterName} = {argumentValue}. Adds to log message "Begin".
+        /// </summary>
+        /// <param name="arguments">Array with given argument to method. Must contain all arguments given to method.</param>
+        /// <param name="methodName">Method namme will be getted in compile time. It should not be specified in method call. </param>
         protected void LogBegin(object[] arguments = null, [CallerMemberName] string methodName = null)
         {
             string args = BuildArgs(arguments);
             logger.LogTrace($"{serviceName}.{methodName}({args}) Begin ");
         }
-
+        /// <summary>
+        /// Writes message to log with name of service, name of method, arguments in format {parameterName} = {argumentValue}. Adds to log message "End".
+        /// </summary>
+        /// <param name="arguments">Array with given argument to method. Must contain all arguments given to method.</param>
+        /// <param name="methodName">Method namme will be getted in compile time. It should not be specified in method call. </param>
         protected void LogEnd(object[] arguments = null, [CallerMemberName] string methodName = null)
         {
             string args = BuildArgs(arguments);
             logger.LogTrace($"{serviceName}.{methodName}({args}) End");
         }
 
+        /// <summary>
+        /// Returns string in format {parameterName} = {argumentValue}. Gets parameters names by getting stack frame.
+        /// </summary>
+        /// <param name="args">Arguments values.</param>
+        /// <returns></returns>
         private string BuildArgs(params object[] args)
         {
             if (args == null)
@@ -55,9 +78,19 @@ namespace Ras.Infastructure.BLL.Proxies.Logging
 
             var arguments = new List<Argument>(args.Length);
 
+            // fill arguments with data from parametersNames and args
             for (int i = 0; i < args.Length; i++)
             {
-                arguments.Add(new Argument(parametersNames[i], args[i].ToString()));
+                string stringValue;
+                if (args[i] != null)
+                {
+                    stringValue = args[i].ToString();
+                }
+                else
+                {
+                    stringValue = "null";
+                }
+                arguments.Add(new Argument(parametersNames[i], stringValue));
             }
 
             var sb = new StringBuilder();
@@ -69,9 +102,19 @@ namespace Ras.Infastructure.BLL.Proxies.Logging
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Represents name of method parameter and given value. 
+        /// </summary>
         public struct Argument
         {
+            /// <summary>
+            /// Parameter name.
+            /// </summary>
             public string Name;
+
+            /// <summary>
+            /// Argument value.
+            /// </summary>
             public string Value;
 
             public Argument(string name, string value)
