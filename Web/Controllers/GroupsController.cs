d0821dc1116564846e541e5ffe7de77b809f1957
Viacheslav Rud <rudviacheslavv@gmail.com>
2018-07-19 10:53:34 +0300

    Improved comments, names.

--- Web/Controllers/GroupsController.cs
+++ Web/Controllers/GroupsController.cs
@@ -23,25 +23,29 @@ namespace Web.Controllers
         }
 
         /// <summary>
-        /// Get sorted list of group with paging
+        ///     Gets list of all groups, sorted by <paramref name="orderBy" />, and returns list with count of items equals
+        ///     <paramref name="itemsPerPage" /> on page with number <paramref name="pageNumber" />. Is used for displaing groups
+        ///     on UI with paging.
         /// </summary>
-        /// <param name="pageNumber">Current page</param>
-        /// <param name="amountItem">Number of items per page</param>
-        /// <param name="orderBy">Parameter for sorting</param>
+        /// <param name="pageNumber">Page number in list of all groups. Count of all pages depends on <paramref name="itemsPerPage" />.</param>
+        /// <param name="itemsPerPage">Count of items will be displayed on page.</param>
+        /// <param name="orderBy">PName of field by which list be sorted.</param>
         /// <returns>List of Group</returns>
-        [HttpGet("getGroups/page={pageNumber}/amount={amountItem}")]
-        public List<GroupInfoViewModel> GetListOfGroup(int pageNumber, int amountItem, string orderBy = "Name")
+        [HttpGet("getGroups/page={pageNumber}/itemsPerPage={itemsPerPage}")]
+        public List<GroupInfoViewModel> GetPagedListOfGroups(int pageNumber, int itemsPerPage, string orderBy = "Name")
         {
-            int skip = (pageNumber - 1) * amountItem;
-            var groups = groupMapper.Map<IEnumerable<GroupDTO>, List<GroupInfoViewModel>>(groupService.GetAll(orderBy, skip, amountItem));
+            int skip = (pageNumber - 1) * itemsPerPage; // TODO move to BLL
+            var groups = groupMapper.Map<IEnumerable<GroupDTO>, List<GroupInfoViewModel>>(groupService.GetAll(orderBy, skip, itemsPerPage));
             return groups;
         }
 
         /// <summary>
-        /// Get sorted list of group with paging and filters
+        ///     Gets list of all groups, filtered by givenValues and sorted by <paramref name="orderBy" />, and returns list with count of items equals
+        ///     <paramref name="itemsPerPage" /> on page with number <paramref name="pageNumber" />. Is used for displaing groups
+        ///     on UI with paging.
         /// </summary>
-        /// <param name="pageNumber">Current page</param>
-        /// <param name="amountItem">Number of items per page</param>
+        /// <param name="pageNumber">Page number in list of all groups. Count of all pages depends on <paramref name="itemsPerPage" />.</param>
+        /// <param name="itemsPerPage">Count of items will be displayed on page.</param>
         /// <param name="orderBy">Parameter for sorting</param>
         /// <param name="name">Parameter for filtering</param>
         /// <param name="startDate">Parameter for filtering</param>
@@ -51,10 +55,10 @@ namespace Web.Controllers
         /// <param name="technologyId">Parameter for filtering</param>
         /// <param name="stageId">Parameter for filtering</param>
         /// <returns></returns>
-        [HttpGet("getGroupsFilters/page={pageNumber}/amount={amountItem}")]
-        public List<GroupInfoViewModel> GetListOfGroup(
+        [HttpGet("getGroupsFilters/page={pageNumber}/itemsPerPage={itemsPerPage}")]
+        public List<GroupInfoViewModel> GetPagedListOfGroup(
             int pageNumber,
-            int amountItem,
+            int itemsPerPage,
             string orderBy = "Name",
             string name = "",
             DateTime? startDate = null,
@@ -64,8 +68,8 @@ namespace Web.Controllers
             int? technologyId = null,
             int? stageId = null)
         {
-            int skip = (pageNumber - 1) * amountItem;
-            var groups = groupMapper.Map<IEnumerable<GroupDTO>, List<GroupInfoViewModel>>(groupService.GetAll(orderBy, skip, amountItem, name, startDate, endDate, cityId, directionId, technologyId, stageId));
+            int skip = (pageNumber - 1) * itemsPerPage; // TODO move to BLL
+            var groups = groupMapper.Map<IEnumerable<GroupDTO>, List<GroupInfoViewModel>>(groupService.GetAll(orderBy, skip, itemsPerPage, name, startDate, endDate, cityId, directionId, technologyId, stageId));
             return groups;
         }
     }
